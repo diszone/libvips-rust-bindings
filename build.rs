9d0eb8353baf0bbb6cfaec1644bcf6c776ec7716
Diszone <diszone@outlook.com>
2023-07-02 13:49:42 +0800

    fix lib path

--- build.rs
+++ build.rs
@@ -1,16 +1,12 @@
 // (c) Copyright 2019-2023 OLX
 fn main() {
-    #[cfg(windows)] 
-    {
+    if cfg!(Windows) {
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", Path::new(&dir).join.("lib"));
+    println!("cargo:rustc-link-search=native={}", std::path::Path::new(&dir).join("lib").to_str().unwrap());
     println!("cargo:rustc-link-lib=libvips");
     println!("cargo:rustc-link-lib=libglib-2.0");
     println!("cargo:rustc-link-lib=libgobject-2.0");
-    }
-
-    #[cfg(not(windows))]
-    {
+    } else {
     println!("cargo:rustc-link-lib=vips");
     println!("cargo:rustc-link-lib=glib-2.0");
     println!("cargo:rustc-link-lib=gobject-2.0");
